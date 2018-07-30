7b4bcb4bcf84efaf4ea541bcb767d90b0f49c822
lh_du <lh_du@Ctrip.com>
2018-07-30 14:44:36 +0800

    指数函数的中缀形式

--- includes/includes.js
+++ includes/includes.js
@@ -1,12 +1,15 @@
 const arr = [1,2,3,NaN];
 
+// Instead of
 if(arr.indexOf(3)>=0){
     console.log(true);
 }
 
+// Use
 if(arr.includes(3)){
     console.log(true);
 }
 
+// PS:note
 arr.includes(NaN);
 arr.indexOf(3);
---
+++ pow/index.js
@@ -0,0 +1,6 @@
+// Instead of 
+console.log(Math.pow(7,2));
+
+
+// Use
+console.log(7**2);
